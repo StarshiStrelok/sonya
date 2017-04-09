535bfd9f121710121d6a3380dd374f3eeeb80529
ss <ss@argo>
2017-04-09 20:21:55 +0300

    Angular CLI

--- sonya-web-admin/src/main/webapp/src/app/app.module.ts
+++ sonya-web-admin/src/main/webapp/src/app/app.module.ts
@@ -2,7 +2,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import {RouterModule} from '@angular/router';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './services/in-memory-data.service';
 
 import {HeroesComponent} from './heroes.component';
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
@@ -22,6 +23,7 @@ import {AppRoutingModule} from './app-routing/app-routing.module'
         BrowserModule,
         FormsModule,
         HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
         AppRoutingModule
     ],
     providers: [HeroService],
