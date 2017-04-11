8701817020b8be995e3a54172ca55b4a121d30a7
ss <ss@argo>
2017-04-11 21:47:14 +0300

    Form validation continue

--- sonya-transport-client/src/main/webapp/src/app/app.module.ts
+++ sonya-transport-client/src/main/webapp/src/app/app.module.ts
@@ -4,14 +4,16 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
-import { TransportProfileComponent } from './form/transport-profile/transport-profile.component';
-import { NumberComponent } from './lib/input/number/number.component';
+import {TransportProfileComponent} from './form/transport-profile/transport-profile.component';
+import {NumberComponent} from './lib/input/number/number.component';
+import { MaxNumberDirective } from './directive/validator/max-number.directive';
 
 @NgModule({
     declarations: [
         AppComponent,
         TransportProfileComponent,
-        NumberComponent
+        NumberComponent,
+        MaxNumberDirective
     ],
     imports: [
         BrowserModule,
