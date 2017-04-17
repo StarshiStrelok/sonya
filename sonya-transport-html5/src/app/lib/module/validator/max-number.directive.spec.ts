84255bc845a0ac4583aee6f2cbffa46639a6829f
ss <ss@argo>
2017-04-17 15:17:05 +0300

    HTML5 project

--- sonya-transport-html5/src/app/lib/module/validator/max-number.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MaxNumberDirective } from './max-number.directive';
-
-describe('MaxNumberDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MaxNumberDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- sonya-transport-html5/src/app/lib/module/validator/min-number.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MinNumberDirective } from './min-number.directive';
-
-describe('MinNumberDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MinNumberDirective();
-    expect(directive).toBeTruthy();
-  });
-});
